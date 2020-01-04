cba5099323d822c288dd7695f4dee361840ac518
pazetic <mr.ahilis@gmail.com>
2020-01-04 17:01:35 +0300

    Кнопка дополнительные материалы для мобильного

--- Landing/script.js
+++ Landing/script.js
@@ -57,17 +57,14 @@ let documents = document.getElementsByClassName('documents')[0],
 
 files_wrap.style.cssText = `
   width: 320px;
-  margin: 0 auto;  
-  display: flex;  
-  flex-wrap: wrap;  
-  align-items: center;  
-  align-content: center;  
+  margin: 0 auto;
   background-color: rgba(0,0,0,.8);  
   border-radius: 10px;  
   padding: 10px;  
   border: 1px solid white;  
   max-height: 90vh;  
-  min-height: 45vh;  
+  min-height: 20vh;  
+  height: auto;
   overflow: auto;
   `; 
 files_wrap_close.style.cssText = `
@@ -83,34 +80,48 @@ files_wrap_close.className = 'fa fa-close icon';
 files_wrap.innerHTML = file;
 files_wrap.prepend(files_wrap_close);
 documents.addEventListener('click',() => {
-  if(!files_wrap_show) {
-    files.style.cssText = `
-      display: flex;
-      align-content: center;
-      flex-wrap: wrap;
-      max-height: 100%;
-      position: fixed;
-      z-index: 2;
-      background-color: rgba(0,0,0,.5);
-      left:0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      `;
-    files.innerHTML = files_wrap.outerHTML;
-    files_wrap_show = true;    
-    files_wrap_close = document.querySelector('.fa-close');
-    files_wrap_close.addEventListener('click', ()=> {filesClose()} );
-  } else {
+  if(files_wrap_show) {
     filesClose()
+  } else {
+    filesShow();
   }
 })
+
+function filesShow() {
+  files.style.cssText = `
+    display: flex;
+    align-content: center;
+    flex-wrap: wrap;
+    max-height: 100%;
+    position: fixed;
+    z-index: 2;
+    background-color: rgba(0,0,0,.5);
+    left:0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    `;
+  files.innerHTML = files_wrap.outerHTML;
+  files_wrap_show = true;    
+  files_wrap_close = document.querySelector('.fa-close');
+  files_wrap_close.addEventListener('click', ()=> {filesClose()} );
+}
 
 function filesClose() {
   files.innerHTML = file;
   files.style.cssText = `
+    backgroundColor: transparent;
     max-height: 30vh;
-    display: block;
-  `;
+    position: relative; `;
+  if(document.body.clientWidth > 767) {
+    files.style.display = 'block';
+  } else {
+    files.style.display = 'none';
+  }
   files_wrap_show = false;
 }
+//обработка нажатия кнопки доп материалов в мобильной версии
+let documents_mobile  = document.querySelector('.files-mobile');
+documents_mobile.addEventListener('click', ()=> {
+  filesShow();
+} )
